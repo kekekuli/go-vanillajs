6be978ea47398b975d3c64b803495d623a48d041
kekekuli <kekekuli@kekekulis-MacBook-Pro.local>
2025-07-28 15:47:37 +0800

    add some API service at client side

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
+import { API } from "./services/API";
+
 window.app = {
   search: (event) => {
     event.preventDefault();
     const q = document.querySelector("input[type=search]").value;
   },
+  api: API
 };
+
--- public/services/API.js
+++ public/services/API.js
@@ -1,5 +1,25 @@
 export const API = {
+  baseURL: "/api",
   getTopMovies: async () => {
-    const response = await fetch("/api/movies/top/");
+    return API.fetch("movies/top/");
   },
+  getRandomMovies: async () => {
+    return API.fetch("movies/random/");
+  },
+  getMovieById: async (id) => {
+    return API.fetch(`movies/${id}`);
+  },
+  searchMovies: async (q, order, genre) => {
+    return API.fetch(`movies/search/`, { q, order, genre });
+  },
+  fetch: async (serviceName, args) => {
+    try {
+      const queryString = args ? new URLSearchParams(args).toString() : "";
+      const response = await fetch(API.baseURL + serviceName + "?" + queryString);
+      const result = await response.json();
+      return result;
+    } catch {
+      console.error(e);
+    }
+  }
 };
